92e2e45c27e8f6f9b4e229af9fd1f8e491d9d4e5
ShuvoChisty <shuvochisti10@gmail.com>
2018-11-08 05:58:55 +0000

    1stnode

---
+++ nodeserver.js
@@ -0,0 +1,25 @@
+var http = require('http');
+ var fs = require('fs');
+ 
+  var server = http.createServer(function(req, res)
+  {
+    res.statusCode = 300;
+    // res.setHeader('Content-Type', 'text/plain');
+    // res.end("Hello World\n");
+    
+        res.setHeader('Content-Type', 'text/html');
+        fs.readFile('index.html', function(err, data){
+        
+        if(err)
+        {
+            return console.log('File Read Error!');
+        }
+        
+        res.end(data);
+        
+        });
+        
+  });
+  server.listen(process.env.PORT, process.env.IP, function(){
+    console.log('Server running');
+});
