cfea02329f938d97c5d81bd4c9ac95fb987350ae
ShuvoChisty <shuvochisti10@gmail.com>
2018-11-12 03:39:10 +0000

    serverjson

--- nodeserver.js
+++ nodeserver.js
@@ -1,5 +1,19 @@
 var http = require('http');
- var fs = require('fs');
+var express=require('express');
+var app=express();
+var server =http.Server(app);
+app.get('/',function(request, response){
+    response.sendFile(__dirname+'/index.html');
+});
+app.get('/about',function(request, response){
+    response.sendFile(__dirname+'/about.html');
+});
+  server.listen(process.env.PORT, process.env.IP, function(){
+    console.log('Server running');
+});
+
+
+ /*var fs = require('fs');
  
   var server = http.createServer(function(req, res)
   {
@@ -19,7 +33,4 @@ var http = require('http');
         
         });
         
-  });
-  server.listen(process.env.PORT, process.env.IP, function(){
-    console.log('Server running');
-});
+  });*/
