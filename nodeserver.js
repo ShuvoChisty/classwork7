c5551d659d0ec1767dbe5bd7350541cccb8c3010
ShuvoChisty <shuvochisti10@gmail.com>
2018-11-22 05:43:07 +0000

    9b

--- nodeserver.js
+++ nodeserver.js
@@ -3,26 +3,48 @@ var express = require('express');
 var app = express();
 var server = http.Server(app);
 var bodyParser = require('body-parser');
-var mongo = require('mongodb');
+// var mongo = require('mongodb');
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var articleSchema = new Schema({
+  title: {
+    type: String,
+    required: "Title Required"
+  },
+  content: {
+    type: String
+  }
+});
+
+var Article = mongoose.model('Article', articleSchema);
 
 var db;
 var db_url = "mongodb://"+process.env.IP+":27017";
-mongo.MongoClient.connect(db_url, {useNewUrlParser:true}, function(err, client){
-  if(err){
-    console.log('Coud not connect to MongoDB');
-  } else{
-    db = client.db('node-cw9');
-  }
-})
+
+// mongo.MongoClient.connect(db_url, {useNewUrlParser:true}, function(err, client){
+//   if(err){
+//     console.log('Coud not connect to MongoDB');
+//   } else{
+//     db = client.db('node-cw9');
+//   }
+// })
 
 
-var save = function(form_data){
-  db.createCollection('articles', function(err, collection){
-    
-  });
-  var collection = db.collection('articles');
-  collection.save(form_data);
-}
+//CW9b
+mongoose.connect(db_url+"/node-cw9");
+mongoose.connection.on('error', function(){
+  console.log('Could not connect to MongoDB');
+});
+
+
+
+
+// var save = function(form_data){
+//   db.createCollection('articles', function(err, collection){
+//   });
+//   var collection = db.collection('articles');
+//   collection.save(form_data);
+// }
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -39,17 +61,14 @@ app.get('/new-article', function(request,response){
 });
 var article = [];
 app.post('/article/create', function(request,response){
-  console.log(request.body);
-  if(!request.body.title){
-    return response.status(400).json({error: "Please add a title"}); 
-  }
-  // article.push(request.body);
-  save(request.body);
-  return response.status(200).json({message: "Article successfully created!"});
+  var new_article = new Article(request.body);
+  new_article.save(function(err,data){
+    if(err)
+      return response.status(400).json({error: "Please add a title"});
+    console.log(data);
+    return response.status(200).json({message: "Article successfully created!"});
+  });
 });
-
-// article.push({title:"Test 1", content:"YOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO1"});
-// article.push({title:"Test 2", content:"YOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO2"});
 
 app.get('/article/list', function(request, response){
   return response.status(200).json({article: article});
@@ -74,6 +93,7 @@ app.get('/article/:articleID', function(request, response){
 //     });
 //     // res.end("Hello World!");
 //   });
+
   server.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server running');
-  });
+});
